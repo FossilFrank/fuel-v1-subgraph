c655c5f7f013354c3c483ebdebac03e1f40b0be9
Fossil Frank <fossilfrank@proton.me>
2023-03-03 02:04:25 +0100

    Basic subgraph

--- src/fuel-v1.ts
+++ src/fuel-v1.ts
@@ -2,7 +2,7 @@ import {
   DepositMade as DepositMadeEvent,
   TokenIndexed as TokenIndexedEvent
 } from "../generated/FuelV1/FuelV1"
-import { DepositMade, TokenIndexed } from "../generated/schema"
+import { DepositMade, Token } from "../generated/schema"
 
 export function handleDepositMade(event: DepositMadeEvent): void {
   let entity = new DepositMade(
@@ -20,15 +20,8 @@ export function handleDepositMade(event: DepositMadeEvent): void {
 }
 
 export function handleTokenIndexed(event: TokenIndexedEvent): void {
-  let entity = new TokenIndexed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  let entity = new Token(event.params.id.toString())
   entity.token = event.params.token
-  entity.id = event.params.id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
   entity.save()
 }
